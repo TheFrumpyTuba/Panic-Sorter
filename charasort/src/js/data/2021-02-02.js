d68ec6c3f385d53ee41dcefe8ac1d9523af7f616
Aidan Sherman <71275183+TheFrumpyTuba@users.noreply.github.com>
2021-02-02 15:34:04 -0500

    Add files via upload

---
+++ charasort/src/js/data/2021-02-02.js
@@ -0,0 +1,512 @@
+dataSetVersion = "2021-02-02"; // Change this when creating a new data set version. YYYY-MM-DD format.
+dataSet[dataSetVersion] = {};
+
+dataSet[dataSetVersion].options = [
+  {
+    name: "Filter by Album",
+    key: "series",
+    tooltip: "Check this to restrict to certain albums.",
+    checked: false,
+    sub: [
+      { name: "A Fever You Can't Sweat Out", key: "1" },
+      { name: "Pretty Odd", key: "2" },
+      { name: "Vices and Virtues", key: "3" },
+      { name: "Too Weird to Live to Young to Die", key: "4" },
+      { name: "Death of a Bachelor", key: "5" },
+      { name: "Pray For the Wicked", key: "6" },
+    ]
+  },
+];
+
+dataSet[dataSetVersion].characterData = [
+  {
+    name: "Introduction",
+    img: "rsDui6M.jpg",
+    opts: {
+      series: ["1"]
+    }
+  },
+  {
+    name: "The Only Difference Between Suicide and Martydom is Press Coverage",
+    img: "rsDui6M.jpg",
+    opts: {
+      series: ["1"] 
+    }
+  },
+  {
+    name: "London Beckoned Songs About Money Written By Machines",
+    img: "rsDui6M.jpg",
+    opts: {
+      series: ["1"] 
+    }
+  },
+  {
+    name: "Nails For Breakfast, Tacks For Snacks",
+    img: "rsDui6M.jpg",
+    opts: {
+      series: ["1"] 
+    }
+  },
+  {
+    name: "Camisado",
+    img: "rsDui6M.jpg",
+    opts: {
+      series: ["1"] 
+    }
+  },
+  {
+    name: "Time to Dance",
+    img: "rsDui6M.jpg",
+    opts: {
+      series: ["1"] 
+    }
+  },
+  {
+    name: "Lying Is The Most Fun a Girl Can Have Without Taking Her Clothes Off",
+    img: "rsDui6M.jpg",
+    opts: {
+      series: ["1"] 
+    }
+  },
+  {
+    name: "Intermission",
+    img: "rsDui6M.jpg",
+    opts: {
+      series: ["1"] 
+    }
+  },
+  {
+    name: "But It's Better If You Do",
+    img: "rsDui6M.jpg",
+    opts: {
+      series: ["1"] 
+    }
+  },
+  {
+    name: "I Write Sins Not Tragedies",
+    img: "rsDui6M.jpg",
+    opts: {
+      series: ["1"] 
+    }
+  },
+  {
+    name: "I Constantly Thank God For Esteban",
+    img: "rsDui6M.jpg",
+    opts: {
+      series: ["1"] 
+    }
+  },
+  {
+    name: "There's A Good Reason These Tables Are Numbered Honey, You Just Haven't Thought Of It Yet",
+    img: "rsDui6M.jpg",
+    opts: {
+      series: ["1"] 
+    }
+  },
+  {
+    name: "Build God, Then We'll Talk",
+    img: "rsDui6M.jpg",
+    opts: {
+      series: ["1"] 
+    }
+  },
+  {
+    name: "We're So Starving",
+    img: "QeIcaMI.jpg",
+    opts: {
+      series:["2"]
+    }
+  },
+  {
+    name: "Nine In The Afternoon",
+    img: "QeIcaMI.jpg",
+    opts: {
+      series:["2"]
+    }
+  },
+  {
+    name: "She's A Handsome Woman",
+    img: "QeIcaMI.jpg",
+    opts: {
+      series:["2"]
+    }
+  },
+  {
+    name: "Do You Know What I'm Seeing?",
+    img: "QeIcaMI.jpg",
+    opts: {
+      series:["2"]
+    }
+  },
+  {
+    name: "That Green Gentleman (Things Have Changed)",
+    img: "QeIcaMI.jpg",
+    opts: {
+      series:["2"]
+    }
+  },
+  {
+    name: "I Have Friends In Holy Spaces",
+    img: "QeIcaMI.jpg.jpg",
+    opts: {
+      series:["2"]
+    }
+  },
+  {
+    name: "Northern Downpour",
+    img: "QeIcaMI.jpg",
+    opts: {
+      series:["2"]
+    }
+  },
+  {
+    name: "When The Day Met The Night",
+    img: "QeIcaMI.jpg",
+    opts: {
+      series:["2"]
+    }
+  },
+  {
+    name: "Pas De Cheval",
+    img: "QeIcaMI.jpg",
+    opts: {
+      series:["2"]
+    }
+  },
+  {
+    name: "The Piano Knows Something I Don't Know",
+    img: "QeIcaMI.jpg",
+    opts: {
+      series:["2"]
+    }
+  },
+  {
+    name: "Behind The Sea",
+    img: "QeIcaMI.jpg",
+    opts: {
+      series:["2"]
+    }
+  },
+  {
+    name: "Folkin' Around",
+    img: "QeIcaMI.jpg",
+    opts: {
+      series:["2"]
+    }
+  },
+  {
+    name: "She Had The World",
+    img: "QeIcaMI.jpg",
+    opts: {
+      series:["2"]
+    }
+  },
+  {
+    name: "From A Mountain In The Middle Of The Cabins",
+    img: "QeIcaMI.jpg",
+    opts: {
+      series:["2"]
+    }
+  },
+  {
+    name: "Mad As Rabbits",
+    img: "QeIcaMI.jpg",
+    opts: {
+      series:["2"]
+    }
+  },
+  {
+    name: "The Ballad Of Mona Lisa",
+    img: "A7YvEZu.jpg",
+    opts: {
+      series:["3"]
+    }
+  },
+  {
+    name: "Let's Kill Tonight",
+    img: "A7YvEZu.jpg",
+    opts: {
+      series:["3"]
+    }
+  },
+  {
+    name: "Hurricane",
+    img: "A7YvEZu.jpg",
+    opts: {
+      series:["3"]
+    }
+  },
+  {
+    name: "Memories",
+    img: "A7YvEZu.jpg",
+    opts: {
+      series:["3"]
+    }
+  },
+  {
+    name: "Trade Mistakes",
+    img: "A7YvEZu.jpg",
+    opts: {
+      series:["3"]
+    }
+  },
+  {
+    name: "Ready To Go(Get Me Out Of My Mind)",
+    img: "A7YvEZu.jpg",
+    opts: {
+      series:["3"]
+    }
+  },
+  {
+    name: "Always",
+    img: "A7YvEZu.jpg",
+    opts: {
+      series:["3"]
+    }
+  },
+  {
+    name: "The Calendar",
+    img: "A7YvEZu.jpg",
+    opts: {
+      series:["3"]
+    }
+  },
+  {
+    name: "Sarah Smiles",
+    img: "A7YvEZu.jpg",
+    opts: {
+      series:["3"]
+    }
+  },
+  {
+    name: "Nearly Witches (Ever Since We Met...)",
+    img: "A7YvEZu.jpg",
+    opts: {
+      series:["3"]
+    }
+  },
+  {
+    name: "This Is Gospel",
+    img: "574nv9i.jpg",
+    opts: {
+      series:["4"]
+    }
+  },
+  {
+    name: "Miss Jackson",
+    img: "574nv9i.jpg",
+    opts: {
+      series:["4"]
+    }
+  },
+  {
+    name: "Vegas Lights",
+    img: "574nv9i.jpg",
+    opts: {
+      series:["4"]
+    }
+  },
+  {
+    name: "Girl That You Love",
+    img: "574nv9i.jpg",
+    opts: {
+      series:["4"]
+    }
+  },
+  {
+    name: "Nicotine",
+    img: "574nv9i.jpg",
+    opts: {
+      series:["4"]
+    }
+  },
+  {
+    name: "Girls/Girls/Boys",
+    img: "574nv9i.jpg",
+    opts: {
+      series:["4"]
+    }
+  },
+  {
+    name: "Casual Affair",
+    img: "574nv9i.jpg",
+    opts: {
+      series:["4"]
+    }
+  },
+  {
+    name: "Far Too Young To Die",
+    img: "574nv9i.jpg",
+    opts: {
+      series:["4"]
+    }
+  },
+  {
+    name: "Collar Full",
+    img: "574nv9i.jpg",
+    opts: {
+      series:["4"]
+    }
+  },
+  {
+    name: "The End Of All Things",
+    img: "574nv9i.jpg",
+    opts: {
+      series:["4"]
+    }
+  },
+  {
+    name: "Victorious",
+    img: "4nZBqm8.jpg",
+    opts: {
+      series:["5"]
+    }
+  },
+  {
+    name: "Don't Threaten Me With A Good Time",
+    img: "4nZBqm8.jpg",
+    opts: {
+      series:["5"]
+    }
+  },
+  {
+    name: "Hallelujah",
+    img: "4nZBqm8.jpg",
+    opts: {
+      series:["5"]
+    }
+  },
+  {
+    name: "Emperor's New Clothes",
+    img: "4nZBqm8.jpg",
+    opts: {
+      series:["5"]
+    }
+  },
+  {
+    name: "Death Of A Bachelor",
+    img: "4nZBqm8.jpg",
+    opts: {
+      series:["5"]
+    }
+  },
+  {
+    name: "Crazy = Genius",
+    img: "4nZBqm8.jpg",
+    opts: {
+      series:["5"]
+    }
+  },
+  {
+    name: "LA Devotee",
+    img: "4nZBqm8.jpg",
+    opts: {
+      series:["5"]
+    }
+  },
+  {
+    name: "Golden Days",
+    img: "4nZBqm8.jpg",
+    opts: {
+      series:["5"]
+    }
+  },
+  {
+    name: "The Good, The Bad And The Dirty",
+    img: "4nZBqm8.jpg",
+    opts: {
+      series:["5"]
+    }
+  },
+  {
+    name: "House Of Memories",
+    img: "4nZBqm8.jpg",
+    opts: {
+      series:["5"]
+    }
+  },
+  {
+    name: "Impossible Year",
+    img: "4nZBqm8.jpg",
+    opts: {
+      series:["5"]
+    }
+  },
+  {
+    name: "(Fuck A) Silver Lining",
+    img: "OiX0THa.jpg",
+    opts: {
+      series:["6"]
+    }
+  },
+  {
+    name: "Say Amen (Saturday Night)",
+    img: "OiX0THa.jpg",
+    opts: {
+      series:["6"]
+    }
+  },
+  {
+    name: "Hey Look Ma, I Made It",
+    img: "OiX0THa.jpg",
+    opts: {
+      series:["6"]
+    }
+  },
+  {
+    name: "High Hopes",
+    img: "OiX0THa.jpg",
+    opts: {
+      series:["6"]
+    }
+  },
+  {
+    name: "Roaring 20s",
+    img: "OiX0THa.jpg",
+    opts: {
+      series:["6"]
+    }
+  },
+  {
+    name: "Dancing's Not A Crime",
+    img: "OiX0THa.jpg",
+    opts: {
+      series:["6"]
+    }
+  },
+  {
+    name: "One Of The Drunks",
+    img: "OiX0THa.jpg",
+    opts: {
+      series:["6"]
+    }
+  },
+  {
+    name: "The Overpass",
+    img: "OiX0THa.jpg",
+    opts: {
+      series:["6"]
+    }
+  },
+  {
+    name: "King Of The Clouds",
+    img: "OiX0THa.jpg",
+    opts: {
+      series:["6"]
+    }
+  },
+  {
+    name: "Old Fashioned",
+    img: "OiX0THa.jpg",
+    opts: {
+      series:["6"]
+    }
+  },
+  {
+    name: "Dying In LA",
+    img: "OiX0THa.jpg",
+    opts: {
+      series:["6"]
+    }
+  },
+];
